cd6122ca59c52d786d0fed113ae98af90261716f
Ryan Gautier <ryangautier2@gmail.com>
2020-09-03 21:07:09 -0500

    add navgroup to all nav items

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,92 +1,71 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from '../../assets/FMTC-Logo.svg';
 import './nav.css';
 
 function Nav() {
-
-  // activeNavGroup will store which nav group is active so it can be closed when needed
-  const [activeNavGroup, setActiveNavGroup] = useState("");
   // toggles the side nav bar
   const toggleNav = () => {
     document.querySelector(".side-nav").classList.toggle("nav-open");
     document.querySelector(".nav-items").classList.toggle("nav-items-open");
   }
 
-  // toggles the selected nav group within the side nav bar
-  const toggleNavGroup = (id) => {
-
-    // if there is an active nav group 
-    if (activeNavGroup !== "") {
-      console.log("closed " + id);
-      closeNavGroup(activeNavGroup);
-    }
-    // if the clicked tab is not already active, make it active
-    if (activeNavGroup !== id) {
-      console.log("opened " + id);
-
-      document.getElementById(id).classList.toggle(id + "-active");
-      let children = document.getElementById(id).children;
-
-      for (let i = 0; i < children.length; i++) {
-        children[i].classList.toggle("hide");
-        children[i].classList.toggle("show");
-      }
-      setActiveNavGroup(id);
-    }
-    // if the clicked tab was active when clicked, it was closed earlier in this function and no tab is active now
-    else {
-      console.log("no active tab");
-      setActiveNavGroup("");
-    }
-  }
-
-  const closeNavGroup = (id) => {
-    document.getElementById(id).classList.remove(id + "-active");
-    let children = document.getElementById(id).children;
-
-    for (let i = 0; i < children.length; i++) {
-      children[i].classList.remove("show");
-      children[i].classList.add("hide");
-    }
-  }
-
   return (
     <div className="position-absolute z-10">
       <header className="navbar navbar-light bg-white fixed-top shadow-sm">
         <div>
+          <i className="fas fa-bars nav-toggle" onClick={() => toggleNav()}></i>
           <img src={logo} alt="FMTC logo" className="logo" />
-          <button className="navbar-toggler" type="button" onClick={() => toggleNav()}>
-            <span className="navbar-toggler-icon"></span>
-          </button>
         </div>
       </header>
 
 
       <nav className="fixed-right side-nav">
         <ul className="nav-items nav flex-column">
-          <li className="nav-item nav-link z-20"
-          data-toggle="collapse" data-target="#dashboard">
+          <li className="nav-item nav-link nav-menu-header"
+            data-toggle="collapse" data-target="#dashboard"
+            aria-expanded="false" aria-controls="dashboard">
             <i className="fas fa-tachometer-alt"></i>Dashboard</li>
           <div className="nav-group collapse" id="dashboard">
             <li className="nav-item nav-link">Profile</li>
             <li className="nav-item nav-link">Account</li>
           </div>
 
-          <li className="nav-item nav-link z-20" ><i className="fas fa-credit-card"></i>Billing</li>
-          <div className="nav-group" id="billing">
-            <li className="nav-item nav-link hide">Subscriptions</li>
-            <li className="nav-item nav-link hide">Customers</li>
-            <li className="nav-item nav-link hide">Invoices</li>
-            <li className="nav-item nav-link hide">Transactions</li>
+          <li className="nav-item nav-link nav-menu-header"
+            data-toggle="collapse" data-target="#billing"
+            aria-expanded="false" aria-controls="billing">
+            <i className="fas fa-credit-card"></i>Billing</li>
+          <div className="nav-group collapse" id="billing">
+            <li className="nav-item nav-link">Subscriptions</li>
+            <li className="nav-item nav-link">Customers</li>
+            <li className="nav-item nav-link">Invoices</li>
+            <li className="nav-item nav-link">Transactions</li>
           </div>
 
-          <li className="nav-item nav-link z-20"><i className="fas fa-briefcase"></i>Catalog</li>
+          <li className="nav-item nav-link nav-menu-header"
+            data-toggle="collapse" data-target="#catalog"
+            aria-expanded="false" aria-controls="catalog">
+            <i className="fas fa-briefcase"></i>Catalog</li>
+          <div className="nav-group collapse" id="catalog">
+            <li className="nav-item nav-link">Deals</li>
+            <li className="nav-item nav-link">Coupons</li>
+          </div>
 
-          <li className="nav-item nav-link z-20"><i className="fas fa-chart-bar"></i>Insights</li>
+          <li className="nav-item nav-link nav-menu-header"
+            data-toggle="collapse" data-target="#tools"
+            aria-expanded="false" aria-controls="tools">
+            <i className="fas fa-tools"></i>Tools</li>
+          <div className="nav-group collapse" id="tools">
+            <li className="nav-item nav-link">Deals</li>
+            <li className="nav-item nav-link">Coupons</li>
+          </div>
 
-          <li className="nav-item nav-link z-20"><i className="fas fa-tools"></i>Tools</li>
-
-          <li className="nav-item nav-link z-20"><i className="fas fa-cog"></i>Settings</li>
+          <li className="nav-item nav-link nav-menu-header"
+            data-toggle="collapse" data-target="#settings"
+            aria-expanded="false" aria-controls="settings">
+            <i className="fas fa-cog"></i>Settings</li>
+          <div className="nav-group collapse" id="settings">
+            <li className="nav-item nav-link">Account Information</li>
+          </div>
 
         </ul>
       </nav>
